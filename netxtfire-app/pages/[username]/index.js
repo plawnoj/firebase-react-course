7da2a9a43a3dfa58199897ee1d3e4fd985824b17
jon <43426006+jcwalp@users.noreply.github.com>
2021-11-16 01:09:44 -0500

    Setup Basic Page routing
    setup file structure inside of pages directory > [brackets] meaning dynamic url fetching and routing > setup next-page react components for each page to load dynamic content to browser

---
+++ netxtfire-app/pages/[username]/index.js
@@ -0,0 +1,7 @@
+export default function UserProfilePage({ }) {
+  return (
+    <main>
+        <h1>Sign Up</h1>
+    </main>
+  )
+}
---
+++ netxtfire-app/pages/admin/index.js
@@ -0,0 +1,7 @@
+export default function AdminPostPage({ }){
+    return (
+        <main>
+            <h1>Edit Post</h1>
+        </main>
+    )
+}

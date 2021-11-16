9f6b741cfa2510ecc540f69d9bedf868d86471c8
jon <43426006+jcwalp@users.noreply.github.com>
2021-11-16 01:24:18 -0500

    added comments to firebase.js

--- netxtfire-app/lib/firebase.js
+++ netxtfire-app/lib/firebase.js
@@ -1,9 +1,12 @@
-import firebase from 'firebase/compat/app'
-import 'firebase/compat/auth';
-import 'firebase/compat/firestore';
-import 'firebase/compat/storage';
+//global file for firebase variables
+import firebase from 'firebase/compat/app' //importing the firebase var
+import 'firebase/compat/auth'; //authentication tool
+import 'firebase/compat/firestore'; //firestore
+import 'firebase/compat/storage'; //storeage
 
-const firebaseConfig = {
+//this is all the stuff that was grabbed from setting up firebase webapp 
+//used to connect to firebase, all of these keys are okay to leave in source 
+const firebaseConfig = { 
     apiKey: "AIzaSyCeXAbMmZ3uZpyFIjPv_gEfFw6CFsRNNxk",
     authDomain: "nextfire-ec7de.firebaseapp.com",
     projectId: "nextfire-ec7de",
@@ -11,11 +14,11 @@ const firebaseConfig = {
     messagingSenderId: "342376647722",
     appId: "1:342376647722:web:420e4cbae6c165e31b0753"
 };
-
+//might have to come back to this to understand what it does
 if (!firebase.apps.length){
     firebase.initializeApp(firebaseConfig);
 }
-
+//exporting the variables to be used globally across the project
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 export const storage = firebase.storage();

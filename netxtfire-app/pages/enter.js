43147580b36c5fa25544afb59174de436375931a
jon <43426006+plawnoj@users.noreply.github.com>
2021-11-17 16:05:56 -0500

    added comments to enter.js

--- netxtfire-app/pages/enter.js
+++ netxtfire-app/pages/enter.js
@@ -14,7 +14,12 @@ export default function Enter(props){
     //3. user signed in, has username <SignOutButton />
     return (
         <main>
-            {user ? !username ? <UsernameForm /> : <SignOutButton /> : <SignInButton />}
+            /* 
+            * come back to this and learn the syntax, but this is basically saying
+            * depending on the current auth state of the window and userContext 
+            * show them the following screen
+            */
+            {user ? !username ? <UsernameForm /> : <SignOutButton /> : <SignInButton />} 
         </main>
     );
 }
@@ -46,32 +51,45 @@ function SignOutButton(){
 //!Username form
 //this is going to turn into a chunky bit of code, make sure to go through and understand whats happening
 function UsernameForm(){
-    const [formValue, setFormValue] = useState('');
+    //this syntax confuses me
+    const [formValue, setFormValue] = useState(''); 
     const [isValid, setIsValid] = useState(false);
     const [loading, setLoading] = useState(false);
     
     const { user, username } = useContext(UserContext);
 
+    /*
+    * this function makes the actual write to the firestore database with the contents of the 
+    * username field and the current user that is signed in
+    * both a user document and a username document are created and stored with all the 
+    * identification information for the end user
+    */
     const onSubmit = async (e) => {
         try {
-            e.preventDefault();
+            e.preventDefault(); //prevents default browser behavior of wanting to refresh page on submit
     
             // Create refs for both documents
-            const userDoc = firestore.doc(`users/${user.uid}`);
-            const usernameDoc = firestore.doc(`usernames/${formValue}`);
+            const userDoc = firestore.doc(`users/${user.uid}`); //pointing to user's collection
+            const usernameDoc = firestore.doc(`usernames/${formValue}`); //pointing to usernames collection
     
             // Commit both docs together as batch write
-            const batch = firestore.batch();
+            const batch = firestore.batch(); //init batch var
+            //setting the writes to be commited to firestore collections for both user's and usernames
             batch.set(userDoc, { username: formValue, photoURL: user.photoURL, displayName: user.displayName });
             batch.set(usernameDoc, { uid: user.uid });
-            console.log(batch);
-            await batch.commit();
+            await batch.commit(); //async await that commits the files to firestore
         } catch (error) {
             console.log('there was an oopsie woopsie fucky wucky' /n);
             console.log(error);
         }
     };
 
+    /*
+    * the state of the username field is asynchronusly being passed thorugh to the on change
+    * function via the onChange={onChange} tag on the input field for username
+    * the declaration onChange = (e) => is what allows this to happen asynchronusly
+    * onChange is what manages the state of the form asyncronusly 
+    */
     const onChange = (e) => { 
         //Force form value typed in form to match correct format
         const val = e.target.value.toLowerCase();
@@ -94,25 +112,21 @@ function UsernameForm(){
 
     }, [formValue]);
 
-    /*
-    * the state of the username field is asynchronusly being passed thorugh to the on change
-    * function via the onChange={onChange} tag on the input field for username
-    * the declaration onChange = (e) => is what allows this to happen asynchronusly
-    */
 
     //* hit database for username match after each debounced change
     //* useCallback is required for debounce to work
     const checkUsername = useCallback(
         debounce(async (username) => {
-            if (username.length >= 3){
-                const ref = firestore.doc('usernames/${username}');
-                const { exists } = await ref.get();
-                console.log('firestore read executed uwu');
-                setIsValid(!exists);
-                setLoading(false);
+            if (username.length >= 3){ //if length >= 3 query the database for existing username
+                //! when referencing firestore collections use ` instead of ' 
+                const ref = firestore.doc(`usernames/${username}`); //pointer for username document in firestore
+                const { exists } = await ref.get(); //exists is a promise attached to grabbing the username data from firestore
+                console.log('firestore read executed uwu'); //console log to show interaction with firestore
+                setIsValid(!exists); //will be a valid username if it does not exist
+                setLoading(false); //update loading state
             }
-        }, 500),
-        []
+        }, 500), //500ms wait time for user to stop typing before querying firestore - debounce allows this
+        [] //redirect for callback maybe?
     );
 
     return (
@@ -139,7 +153,7 @@ function UsernameForm(){
         )
     );
 }
-
+//*Function that allows for the dynamic message listed below username field 
 function UsernameMessage({ username, isValid, loading }){
     if (loading){
         return <p>checking...</p>;

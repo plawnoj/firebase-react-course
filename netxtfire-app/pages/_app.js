77aa99a2ca192eb38e08d52374d73aaa3f4feb21
jon <43426006+plawnoj@users.noreply.github.com>
2021-11-16 20:01:23 -0500

    hehe i broke a lot of shit here
    issue installing react-hot-toast > troubleshooting the issue > tried reinstalling deps. > not working > messing around with nvm and diff node versions > committing this version to cut down on bloat

--- netxtfire-app/pages/_app.js
+++ netxtfire-app/pages/_app.js
@@ -1,11 +1,14 @@
-import '../styles/globals.css'
-import Navbar from '../components/Navbar'
+import React from 'react';
+import '../styles/globals.css';
+import Navbar from '../components/Navbar';
+import toast, { Toaster } from 'react-hot-toast';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
     <Navbar />
     <Component {...pageProps} />
+    <Toaster />
     </>
   )
 }
--- netxtfire-app/pages/index.js
+++ netxtfire-app/pages/index.js
@@ -1,13 +1,17 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import React from 'react';
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
 
-import Loader from '../components/Loader'
+import Loader from '../components/Loader';
+import toast from 'react-hot-toast';
 
 export default function Home() {
   return (
     <div>
-      <Loader show />
+      <button onClick={() => toast.success('hello toast!')}>
+        click me!
+      </button>
     </div>
   )
 }
